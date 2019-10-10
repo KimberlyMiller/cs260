ce5198962670758dfc71319aa7dc4d3559b290a0
KimberlyMiller <millerkimberly62@gmail>
2019-10-10 15:56:53 -0700

    test github push..

--- src/edu/bluecc/cs260/stacks/ArrayStack.java
+++ src/edu/bluecc/cs260/stacks/ArrayStack.java
@@ -6,24 +6,35 @@ package edu.bluecc.cs260.stacks;
      * @param item - the value to be pushed onto the stack
      * @return the value
      */
-    public String push(String item);
+    public String push(String item) {
+      a[++top} = item;
+      return item;
+    }
 
+    public String push(String item) {
+      a[++top] = item;
+      return item;
+    }
     /**
      * Pops a value off the stack
      * @return the popped value
      */
-    public String pop();
-
+    public String pop() {
+      return a[top--];
+    }
     /**
      * Returns the top value without removing it
      * @return the top value
      */
-    public E peek();
-
+    public String peek() {
+      return a[top];
+    }
     /**
      * Tests if this stack is empty
      * @return true if the stack is empty, false otherwise
      */
-    public boolean isEmpty();
-    // test github push...
+    public boolean isEmpty() {
+      return < 0;
+    }
+
 }
