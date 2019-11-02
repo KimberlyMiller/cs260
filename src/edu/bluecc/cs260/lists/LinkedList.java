ef8e7a06687d0924073dfbb3fc4da22ea7fe2439
Kimberly Miller <millerkimberly62@gmail.com>
2019-11-02 13:45:42 -0700

    homework Nov 2 - LinkedList project

--- src/edu/bluecc/cs260/lists/LinkedList.java
+++ src/edu/bluecc/cs260/lists/LinkedList.java
@@ -1,7 +1,5 @@
 package edu.bluecc.cs260.lists;
 
-import java.sql.SQLOutput;
-
 // TODO add double linking to the mothod and then leave this comment for the instructor
 
 public class LinkedList implements List {
@@ -31,11 +29,42 @@ public class LinkedList implements List {
     private int size = 0;
 
     /**
+     * Retrieve a value from the list at a given index
+     * Throw an IndexOutOfBoundsException if the index is invalid
+     * implements RETRIEVE(p) -> v in ADT LIST
      *
+     * @param index the index in the list; 0 is first index
+     * @return the value at the given index location
+     */
+    public String get(int index) {
+        if (index < 0 || index >= size)  // this checks to see if this index is out of bounds
+            throw new IndexOutOfBoundsException(index + " is an invalid index");
+        Node temp =  first ; // this is not a new object in memory, just a temporary pointer to be moved.
+        for (int i = 0; i < index; i++)
+            temp = temp.getNext(); // this keeps moving the temp pointer to the next position in the list
+        return temp.getValue();
+    }
+
+    /**
+     * Return the first indext in the list with the given value, or -1 if it's not found
+     * Implements the LOCATE(v) -> p in ADT LIST
+     *
+     * @param value
+     * @return the index where it was found, or -1 if not in the list
+     */
+    public int find(String value){
+    // set a temporary pointer to the first element
+    // move the pointer until we find the value
+        // TODO write this
+        return -1;
+    }
+
+    /**
      * Appends the specified element to the end of the list
-     *
      * implements APPEND(v) in ADT LIST
+     *
      * @param value value to be appended to this list
+     * @return true if the element is successfully added, false if the list is full
      */
     public boolean add(String value) {
         // create new node
@@ -51,14 +80,21 @@ public class LinkedList implements List {
         return true;
     }
 
+    /**
+     * Throw an IndexOutOfBoundsException if the index is invalid
+     * Implements INSERT(p,v)
+     *
+     * @param index and val
+     * @return
+     */
     @Override
     public void add(int index, String val) {
         // TODO write this
     }
 
     /**
-     * Implements the DELETE(v) operation of ADT LIST
      * Removes the first occurrence of the given value from the list, if it exists
+     * Implements the DELETE(v) operation of ADT LIST
      *
      * @param value the values to be removed
      * @return true the value was removed, false if wasn't found in the list
@@ -86,81 +122,81 @@ public class LinkedList implements List {
         return false;  // if it is not in the list
     }
 
+    /**
+     * return the value that's removed; throw an IndexOutOfBoundsException if the index is invalid
+     * Implements DELETE(p)
+     *
+     * @param index
+     * @return return the value that's removed;
+     */
     @Override
     public String remove(int index) {
-        // TODO write this
-        return null;
-    }
-
-    // DELETE(p)
-    // return the value that's removed; throw an INdexOutOfBoundsException if the index is invalid
-    // TODO public String remove(int index) {}
-
-    /**
-     *
-     * Retrieve a value from the list at a given index
-     * implements RETRIEVE(p) -> v (value) in ADT LIST
-     *
-     * @param index the index in the list; 0 is first index
-     * @return the value at the given index location
-     */
-    public String get(int index) {
-        if (index < 0 || index >= size)  // this checks to see if this index is out of bounds
-            throw new IndexOutOfBoundsException(index + " is an invalid index");
-        Node temp =  first ; // this is not a new object in memory, just a temporary pointer to be moved.
-        for (int i = 0; i < index; i++)
-            temp = temp.getNext(); // this keeps moving the temp pointer to the next position in the list
-        return temp.getValue();
+        return null; // TODO write this for dynamic memory
     }
 
     /**
-     * Implements LOCATE(v) → p
-     * Return the first index in the list with the given value, or -1 if it's not found
+     * This method gets the value of the end variable -1 to indicate the list size
+     * Implements LENGTH() → int
      *
-     * @param val
-     * @return
+     * @return return the size of the list
      */
     @Override
-    public int find(String val) {
-        // TODO write this
-        return 0;
-    }
-
-    /**
-     *
-     * Implements the LOCATE(v) -> p operation of ADT LIST
-     * Return the first indext in the list with the given value, or -1 if it's not found
-     *
-     * @param value the value to look for
-     * @return the index where it was found, or -1 if not in the list
-     */
-    // TODO public int find(String value){
-       // set a temporary pointer to the first element
-        // move the pointer until we find the value
-
-        // return -1 if not found
-    //}
-
-
     public int size() {
-        // TODO write this
         return size;
     }
 
+    /**
+     * This method checks to see if the list is empty
+     * This was not in the Abstract List of Operations
+     *
+     * @return true if the list is empty, false if it's not
+     */
     @Override
     public boolean isEmpty() {
-        // TODO write this
+        if (size() <= 0)
+            return true;
         return false;
     }
 
+    /**
+     * This method checks to see if value is in the list
+     * This was not in the Abstract List of Operations
+     *
+     * @param value
+     * @return true if the value is in the list, false if it's not
+     */
     @Override
     public boolean contains(String value) {
-        // TODO write this
+        int index = find(value);
+        if (index != -1) {
+            return true;
+        }
         return false;
     }
 
+    /**
+     * This method removes everything from the list
+     * Implements DELETE_ALL()
+     */
     @Override
     public void clear() {
+        size = -1;
+    } // TODO double check if -1 is valid
 
+    /**
+     * Convert the contents into a single string with values separated by commas.
+     * This was not in the Abstract List of Operations
+     *
+     * @return comma separated element in the list
+     */
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        if (end < 0)
+            return "";
+        for (int i = 0; i < end; i++) {
+            s.append(a[i]).append(",");
+        }
+        return s.append(a[end]).toString();
     }
 }
