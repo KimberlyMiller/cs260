bbd2fdecc2da750b64179c922cf8806fb37151a7
Kimberly Miller <millerkimberly62@gmail.com>
2019-11-30 21:19:58 -0800

    expression tester

--- src/edu/bluecc/cs260/stacks/ExpressionTester.java
+++ src/edu/bluecc/cs260/stacks/ExpressionTester.java
@@ -1,69 +1,73 @@
 package edu.bluecc.cs260.stacks;
 
 
+import edu.princeton.stlib.StdIn;
+import edu.princeton.stlib.StdOut;
+
 public class ExpressionTester {
 
     public static void main(String[] args) {
 
         ArrayStack arrayStackExpression = new ArrayStack();
-        expressionTest(arrayStackExpression);
+//        System.out.println("Type an expression to validate and end with 'cancel");
+//        if (lines.equals("q")
+//        System.out.println("this q....");
+        String[] lines = StdIn.readAllLines();
+        // TODO figure out how to get the file to cancel
 
-//        LinkedStack linkedStackExpression = new LinkedStack();
-//        expressionTest(linkedStackExpression);
 
+        expressionTest(arrayStackExpression, lines);
     }
 
-    public static void expressionTest(ArrayStack<String> stack) {
-        //String s = "([Hello])";
-       // String s =  "(10)";
-       //  String s = "{([42])([27])[]}";
-       // String s = "[[[{{{((()))}}}]]]";
-        // String s = ")";
-        String s = "[()]]";
+    public static void expressionTest(ArrayStack<String> stack, String[] lines) {
+         String s = "([Hello])"; // valid expression
+//         String s = "(10)"; // valid expression
+//         String s = "([Hello])"; // valid expression
+//         String s = "{([42])([27])[]}"; // valid expression
+//         String s = "[[[{{{((()))}}}]]]"; // valid expression
 
-            // trace: System.out.println(stack.isEmpty());
-            String temp = "";
-        int isAdded = 0;
+//        String s = ")"; // invalid expression
+//         String s = "[()]]"; // invalid expression
+//         String s = "{17(23)"; // invalid expression
+//    String s = "((([[[]]]))))"; // invalid expression
 
+        String[] expressions = new String[lines.length];
+
+        for (int i = 0; i < lines.length; i++) {
+            String[] parts = lines[i].split(" ");
+            String expression = parts[0];
+            expressions[i] = expression;
+        }
+
+        String temp = "";
+        int  stackWasUsed = 0;
+        int notMatched = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             temp = Character.toString(c);
 
-
             if (temp.equals("[") || temp.equals("(") || temp.equals("{")) {
                 stack.push(temp);
-                System.out.println(stack.peek());
-                isAdded = isAdded + 1;
+                stackWasUsed = 1;
             }
 
             if (!stack.isEmpty()) {
                 String top = stack.peek();
-                // trace:  System.out.println(top); System.out.println("Top: " + top); System.out.println("Temp: " + temp);
                 if (top.equals("[") && temp.equals("]")) {
                     stack.pop();
-                    // trace:
-                    System.out.println("[...........");
                 } else if (top.equals("(") && temp.equals(")")) {
                     stack.pop();
-                    // trace:
-                    System.out.println("(...........");
                 } else if (top.equals("{") && temp.equals("}")) {
                     stack.pop();
-                    // trace:
-                    System.out.println("{...........");
-                } else if (temp.equals("]") || temp.equals(")") || temp.equals("}"))
-                    System.out.println("Inside test " + temp);
-
+                }
+           } else if (temp.equals("]") || temp.equals(")") || temp.equals("}")) {
+                notMatched = notMatched + 1;
             }
 
-            System.out.println("Temp: " + temp);
         }
-
-        System.out.println(stack.isEmpty());
-        if (stack.isEmpty() && isAdded != 0) {
-            System.out.println("temp s" + " is a vaild expresion");
-        }
-        else if (!stack.isEmpty() || isAdded == 0)
-            System.out.println("ERROR: " + "temp s" + " is invalid");
+        if (stack.isEmpty() && stackWasUsed == 1 && notMatched == 0)
+            System.out.println(s + " is a vaild expresion");
+        else if (!stack.isEmpty() || stackWasUsed == 0 || notMatched > 0)
+            System.out.println("ERROR: " + s + " is invalid");
     }
 }
