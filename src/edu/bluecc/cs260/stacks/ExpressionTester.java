bed07bca729ae298091d0367c4ebae0afc182d98
Kimberly Miller <millerkimberly62@gmail.com>
2019-12-02 19:54:20 -0800

    expression tester finished

--- src/edu/bluecc/cs260/stacks/ExpressionTester.java
+++ src/edu/bluecc/cs260/stacks/ExpressionTester.java
@@ -1,49 +1,30 @@
 package edu.bluecc.cs260.stacks;
 
-
-import edu.princeton.stlib.StdIn;
-import edu.princeton.stlib.StdOut;
+import java.util.Scanner;
 
 public class ExpressionTester {
 
     public static void main(String[] args) {
 
         ArrayStack arrayStackExpression = new ArrayStack();
-//      System.out.println("Type an expression to validate and end with 'cancel");
-//      if (lines.equals("q")
-//        System.out.println("this q....");
 
-
-
-
-        System.out.print("Command-line parameters:");
-        String[] parameters = scanner.nextLine().split("\\s+");
-
-
-
-//        System.out.print("Command-line parameters: \n");
-//        //String stop = "cancel";
-//
-//        String[] lines = StdIn.readAllLines();
-//        System.out.println(lines[0]);
-//        //while (!stop.equals("cancel")) {
-//           // System.out.println(stop);
-//            String[] expressions = new String[lines.length];
-//
-//
-//            for (int i = 0; i < lines.length; i++) {
-//                String[] parts = lines[i].split(" ");
-//                String expression = parts[0];
-//                expressions[i] = expression;
-//            }
-//        //}
-        // TODO figure out how to get the file to cancel
+        Scanner scanner = new Scanner(System.in);
+        String string = new String("");
+        String[] lines = new String[100000];
+        int i = 0;
+        while (!string.equalsIgnoreCase("cancel")) {
+            System.out.println("Type an expression to validate and end with 'cancel");
+            string = scanner.nextLine();
+            if (!string.equalsIgnoreCase("cancel"))
+                lines[i++] = string;
+        }
+        scanner.close();
 
         expressionTest(arrayStackExpression, lines);
     }
 
     public static void expressionTest(ArrayStack<String> stack, String[] lines) {
-         String s = "([Hello])"; // valid expression
+        //     String s = "([Hello])"; // valid expression
 //         String s = "(10)"; // valid expression
 //         String s = "([Hello])"; // valid expression
 //         String s = "{([42])([27])[]}"; // valid expression
@@ -54,39 +35,42 @@ public class ExpressionTester {
 //         String s = "{17(23)"; // invalid expression
 //    String s = "((([[[]]]))))"; // invalid expression
 
-
-
-
-
         String temp = "";
-        int  stackWasUsed = 0;
+        String s = "";
+        int stackWasUsed = 0;
         int notMatched = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            temp = Character.toString(c);
+        for (int j = 0; j < lines.length; j++) {
+            s = lines[j];
+            if (lines[j] != null){
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                temp = Character.toString(c);
 
-            if (temp.equals("[") || temp.equals("(") || temp.equals("{")) {
-                stack.push(temp);
-                stackWasUsed = 1;
-            }
-
-            if (!stack.isEmpty()) {
-                String top = stack.peek();
-                if (top.equals("[") && temp.equals("]")) {
-                    stack.pop();
-                } else if (top.equals("(") && temp.equals(")")) {
-                    stack.pop();
-                } else if (top.equals("{") && temp.equals("}")) {
-                    stack.pop();
+                if (temp.equals("[") || temp.equals("(") || temp.equals("{")) {
+                    stack.push(temp);
+                    stackWasUsed = 1;
                 }
-           } else if (temp.equals("]") || temp.equals(")") || temp.equals("}")) {
-                notMatched = notMatched + 1;
+
+                if (!stack.isEmpty()) {
+                    String top = stack.peek();
+                    if (top.equals("[") && temp.equals("]")) {
+                        stack.pop();
+                    } else if (top.equals("(") && temp.equals(")")) {
+                        stack.pop();
+                    } else if (top.equals("{") && temp.equals("}")) {
+                        stack.pop();
+                    }
+                } else if (temp.equals("]") || temp.equals(")") || temp.equals("}")) {
+                    notMatched = notMatched + 1;
+                }
+
             }
 
+            if (stack.isEmpty() && stackWasUsed == 1 && notMatched == 0)
+                System.out.println(s + " is a vaild expresion");
+            else if (!stack.isEmpty() || stackWasUsed == 0 || notMatched > 0)
+                System.out.println("ERROR: " + s + " is invalid");
         }
-        if (stack.isEmpty() && stackWasUsed == 1 && notMatched == 0)
-            System.out.println(s + " is a vaild expresion");
-        else if (!stack.isEmpty() || stackWasUsed == 0 || notMatched > 0)
-            System.out.println("ERROR: " + s + " is invalid");
+        }
     }
 }
