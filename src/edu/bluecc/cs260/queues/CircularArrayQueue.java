77fa40fbacee0776908c446411e90d07f831577b
KimberlyMiller <millerkimberly62@gmail>
2019-12-04 16:02:13 -0800

    Circular Array Queue class if finished

--- src/edu/bluecc/cs260/queues/CircularArrayQueue.java
+++ src/edu/bluecc/cs260/queues/CircularArrayQueue.java
@@ -5,30 +5,40 @@ import java.util.NoSuchElementException;
 public class CircularArrayQueue<E> implements Queue<E> {
 
   public static void main(String[] args) {
-
   }
 
   private E[] a = (E[])(new Object[1000]);
   // values have to match the contracts.  Only cast the data types when pulling the information at runtime.
 
   private int front = 0;
-  private int back = 0;
-  private int end = -1;
+  private int back = -1;
 
   /**
    * Adds an element to the queue. Returns false if the queue is full.
+   * (back == a.length-1) means queue is full, the array has hit its max value // a.length-1
    * @param value - the value to add
    * @return - true if the element was added, false otherwise
    */
   @Override
   public boolean add(E value) {
-    // if the queue is full, return false
-    // if back is one less than front -- the queue is full
-
-    // put the element in "back", then  increment back (circularly, usuing the modulo operator)
-    return false;
+    if (back == a.length-1)
+      return false;
+    else
+      a[++back] = value;
+    back = (back + 1) % a.length;
+    return true;
   }
 
+  /**
+   * Returns the value at the front of the queue without removing it
+   * the front of the queue is always at index zero, we just want to know what this is
+   * @return - the value at the front of the queue
+   */
+  public E peek() {
+    if (isEmpty())
+      throw new NoSuchElementException("Queue is empty");
+    return a[front];
+  }
 
   /**
    * @return true if this queue contains no elements
@@ -40,17 +50,22 @@ public class CircularArrayQueue<E> implements Queue<E> {
     return null;
   }
 
+  /**
+   * if the queue is empty, then front and back are the same
+   */
   @Override
   public boolean isEmpty() {
-    // if the queue is empty, then front and back are the same
+    if (front == back)
+      return true;
     return false;
   }
 
   /**
    * Clears the queue
+   * the queue should be empty after this method is executed.
    */
   @Override
   public void clear() {
-    // if the queue should be empty after this method is executed.
+    back = -1;
   }
 }
