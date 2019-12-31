7ba2d40e7d0060eb248a4afcc6ac91cd94776a0d
KimberlyMiller <millerkimberly62@gmail>
2019-12-31 14:36:38 -0800

    Updated the MapProgram

--- src/edu/bluecc/cs260/hashtable/MapProgram.java
+++ src/edu/bluecc/cs260/hashtable/MapProgram.java
@@ -28,30 +28,59 @@ import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class MapProgram {
 
   public static void main(String[] args) {
-    Map<Integer, Student> studentMap = readStudents("students.txt");
+    Map<Integer, Student> studentMap = readStudents("C:\\01 working on hard drive - remove files not in use\\BMCC\\cs260\\src\\edu\\bluecc\\cs260\\hashtable\\students.txt");
     System.out.println(studentMap.size());
     showScores(studentMap);
     double average = calculateAverage(studentMap);
     System.out.println("Average is: " + average);
   }
 
+  private List<Map<Integer, Student>>[] a = (List<Map<Integer, Student>>[]) new List[100];
+  private int size = 0;
+
+  /**
+   * Calculate the average of the scores
+   * @param studentMap
+   * @return the scores average
+   */
   private static double calculateAverage(Map<Integer, Student> studentMap) {
     // Your code here. Loop over all keys, getting the student score
     // for each and adding to a variable representing the total
+
+    int location = getLocation(student.getId());
+
+    double average = 0;
+    for (Map<Integer, Student> student : a[location]) {
+        average = studentMap.getID() + average;
+    }
+    average = studentMap.getID()/ a.length;
+    return average;
+  }
+
+  /**
+   * Apply the hash function to this key
+   * @param key the key
+   * @return the location for this key in the underlying array
+   */
+  private int getLocation(Map key) {
+    return Math.abs(key.hashCode()) % a.length;
   }
 
   private static void showScores(Map<Integer, Student> studentMap) {
     // Your code here. Loop over each key, showing the student name and score for each entry.
+    int location = getLocation(studentMap.getId());
 
+    for (Map<Integer, Student> student : a[location]) {
+      System.out.println("Student score: " + studentMap.getScore() + ", " + studentMap.getName());
+      }
   }
-
-
 
   private static Map<Integer,Student> readStudents(String filename) {
     Map<Integer,Student> map = new HashMap<>();
